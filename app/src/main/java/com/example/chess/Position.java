3a5a2f7bd772362f36e6302376143cc35ad891a6
nirajtaliya123 <69964346+nirajtaliya123@users.noreply.github.com>
2021-01-31 16:40:41 +0530

    first commit

---
+++ app/src/main/java/com/example/chess/Position.java
@@ -0,0 +1,27 @@
+package com.example.chess;
+
+
+import com.example.chess.Pieces.Piece;
+
+/**
+ * Created by DIPANSH KHANDELWAL on 03-06-2017
+ */
+
+public class Position {
+    private Piece piece;
+
+
+    Position(Piece piece ) {
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
+
+    }
+
+    void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+}
